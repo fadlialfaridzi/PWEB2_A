3986bd6c327fc962da5100f4a9e93d2843e7d68c
fadlialfaridzi <fadlialfaridzi93@gmail.com>
2025-06-13 15:52:36 +0700

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET beranda / index page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Login - Kosand' });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register - Kosand' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Register - Kosand' });
+});
+
+/* GET role page. */
+router.get('/role', function(req, res, next) {
+  res.render('role', { title: 'Register - Kosand' });
+});
+
+module.exports = router;
